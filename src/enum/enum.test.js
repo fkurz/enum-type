bbdc4675d3d1cf62f67820058bbad487a4841853
Friedrich <friedrich.kurz@live.com>
2018-08-28 19:44:11 +0200

    Add test case for use of enum values in `switch`; restructure tests

--- src/enum/enum.test.js
+++ src/enum/enum.test.js
@@ -1,16 +1,27 @@
 import { Enum } from "./";
 
 describe("Enum methods", () => {
-  const countryCodeEnumArray = [
-    { key: "AU", value: "Australia (+61)" },
-    { key: "CA", value: "Canada (+1)" },
-    { key: "CH", value: "Switzerland (+41)" },
-    { key: "DE", value: "Germany (+49)" },
-    { key: "FR", value: "France (+33)" },
-    { key: "GB", value: "GB (+44)" },
-    { key: "NL", value: "NL (+31)" },
-    { key: "US", value: "US (+1)" }
+  const countryCodeArray = [
+    ["AU", "Australia (+61)"],
+    ["CA", "Canada (+1)"],
+    ["CH", "Switzerland (+41)"],
+    ["DE", "Germany (+49)"],
+    ["FR", "France (+33)"],
+    ["GB", "GB (+44)"],
+    ["NL", "NL (+31)"],
+    ["US", "US (+1)"]
   ];
+
+  const countryCodeObject = {
+    AU: "Australia (+61)",
+    CA: "Canada (+1)",
+    CH: "Switzerland (+41)",
+    DE: "Germany (+49)",
+    FR: "France (+33)",
+    GB: "GB (+44)",
+    NL: "NL (+31)",
+    US: "US (+1)"
+  };
 
   const countryCodeEnumObject = {
     AU: 0,
@@ -32,22 +43,27 @@ describe("Enum methods", () => {
   };
 
   describe("Static methods", () => {
-    test("`Enum.from` constructs enum object", () => {
-      const e = Enum.from(countryCodeEnumArray);
+    test("`Enum.fromArray` constructs enum object correctly", () => {
+      const e = Enum.fromArray(countryCodeArray);
+      expect(e).toMatchObject(countryCodeEnumObject);
+    });
+
+    test("`Enum.fromObject` constructs enum object correctly", () => {
+      const e = Enum.fromObject(countryCodeObject);
       expect(e).toMatchObject(countryCodeEnumObject);
     });
   });
 
   describe("Methods", () => {
-    const e = Enum.from(countryCodeEnumArray);
+    const e = Enum.fromArray(countryCodeArray);
 
     test("`Enum.prototype.keys`", () => {
-      const keys = countryCodeEnumArray.map(({ key }) => key);
+      const keys = countryCodeArray.map(([key, value]) => key);
       expect(e.keys()).toEqual(keys);
     });
 
     test("`Enum.prototype.entries`", () => {
-      const entries = countryCodeEnumArray.reduce((result, { key, value }) => {
+      const entries = countryCodeArray.reduce((result, [key, value]) => {
         result[key] = value;
         return result;
       }, {});
@@ -55,12 +71,72 @@ describe("Enum methods", () => {
     });
 
     test("`Enum.prototype.values`", () => {
-      const values = countryCodeEnumArray.map(({ value }) => value);
+      const values = countryCodeArray.map(([_, value]) => value);
       expect(e.values()).toEqual(values);
     });
 
     test("`Enum.prototype.keyOf`", () => {
       expect(e.keyOf("France (+33)")).toEqual("FR");
+    });
+  });
+
+  describe("Common use-cases", () => {
+    const CountryCodes = Enum.fromObject(countryCodeObject);
+
+    test("`Enum.prototype[Symbol.iterator]` works correctly for empty enum", () => {
+      const Empty = new Enum();
+      for (const element of Empty) {
+        expect(false).toBe(true);
+      }
+    });
+
+    test("`Enum.prototype[Symbol.iterator]` iterates over whole enum`", () => {
+      const entries = countryCodeArray.reduce((result, [key, value]) => {
+        result[key] = value;
+        return result;
+      }, {});
+      const result = {};
+      for (const [key, value] of CountryCodes) {
+        result[key] = value;
+      }
+      expect(result).toEqual(entries);
+    });
+
+    test("`Enum` values can be used in `switch`", () => {
+      const keys = CountryCodes.keys();
+      const randomEnumValue = Math.round(Math.random() * keys.length);
+      let key;
+
+      switch (randomEnumValue) {
+        case CountryCodes.AU:
+          key = "AU";
+          break;
+        case CountryCodes.CA:
+          key = "CA";
+          break;
+        case CountryCodes.CH:
+          key = "CH";
+          break;
+        case CountryCodes.DE:
+          key = "DE";
+          break;
+        case CountryCodes.FR:
+          key = "FR";
+          break;
+        case CountryCodes.GB:
+          key = "GB";
+          break;
+        case CountryCodes.NL:
+          key = "NL";
+          break;
+        case CountryCodes.US:
+          key = "US";
+          break;
+        default:
+          expect(true).toBe(false);
+      }
+
+      expect(key).toEqual(CountryCodes.keyOf(CountryCodes[randomEnumValue]));
     });
   });
 });
